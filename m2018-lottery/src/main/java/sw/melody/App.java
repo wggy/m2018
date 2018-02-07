7708b8879b158d29a9e788319c51a0b84c698958
melody <137987284@qq.com>
2018-02-07 18:03:50 +0800

    修改项目架构，分模块

---
+++ m2018-lottery/src/main/java/sw/melody/App.java
@@ -0,0 +1,34 @@
+package sw.melody;
+
+/**
+ * Hello world!
+ * <p>
+ * …or create a new repository on the command line
+ * <p>
+ * git提交方式
+ * <p>
+ * 1.从本地仓库提交
+ * 创建本地仓库
+ * git init
+ * 提交本地分支到暂存区
+ * git add README.md
+ * 提交本地分支
+ * git commit -m "first commit"
+ * 本地分支关联github远程仓库
+ * git remote add origin https://github.com/year201412/m2018.git
+ * 提交到github仓库
+ * git push -u origin master
+ * <p>
+ * 2.从本地仓库提交到已知github仓库
+ * …or push an existing repository from the command line
+ * <p>
+ * 关联githu远程仓库
+ * git remote add origin https://github.com/year201412/m2018.git
+ * 提交到github仓库
+ * git push -u origin master
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ m2018-segment/src/main/java/sw/melody/algorithm/classic/QuickSort.java
@@ -0,0 +1,19 @@
+package sw.melody.algorithm.classic;
+
+
+/***
+ * 快速排序
+ *
+ * 是冒泡排序的升级版。
+ * 基本思想：
+ * 1.选取基准元素，一般默认为第一个数组元素
+ * 2.将
+ *
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+    }
+
+}
