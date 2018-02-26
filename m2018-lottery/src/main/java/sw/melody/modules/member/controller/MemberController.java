44a46155a72a4bc87c6727523ad5b94d6d209d44
melody <137987284@qq.com>
2018-02-26 18:03:35 +0800

    创建成员模块

---
+++ m2018-lottery/src/main/java/sw/melody/modules/member/controller/MemberController.java
@@ -0,0 +1,59 @@
+package sw.melody.modules.member.controller;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sw.melody.modules.member.entity.MemberEntity;
+import sw.melody.modules.member.service.MemberService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * Created by ping on 2018/2/26
+ */
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @ApiOperation(value="获取成员列表")
+    @RequestMapping(value="/list", method= RequestMethod.GET)
+    public List<MemberEntity> getUserList() {
+        return memberService.queryAllMembers();
+    }
+
+    @ApiOperation(value="创建成员", notes="根据Member对象创建成员")
+    @ApiImplicitParam(name = "member", value = "成员详细实体member", required = true, dataType = "MemberEntity")
+    @RequestMapping(value="/create", method=RequestMethod.POST)
+    public String createMember(@RequestBody MemberEntity user) {
+        if (StringUtils.isEmpty(user.getMember()) || user.getStatus() == null) {
+            return "failed";
+        }
+        memberService.create(user.getMember(), user.getStatus());
+        return "success";
+    }
+
+    @ApiOperation(value="获取成员详细信息", notes="根据url的id来获取成员详细信息")
+    @ApiImplicitParam(name = "id", value = "成员ID", required = true, dataType = "Long")
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public MemberEntity getMember(@PathVariable Long id) {
+        if (id == null) {
+            return null;
+        }
+        return memberService.queryById(id);
+    }
+
+    @ApiOperation(value="删除成员", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        memberService.deleteAllMembers();
+        return "success";
+    }
+
+}
---
+++ m2018-lottery/src/main/java/sw/melody/modules/member/entity/MemberEntity.java
@@ -0,0 +1,13 @@
+package sw.melody.modules.member.entity;
+
+import lombok.Data;
+
+/***
+ * Created by ping on 2018/2/26
+ */
+@Data
+public class MemberEntity {
+    private Integer id;
+    private String member;
+    private Integer status;
+}
