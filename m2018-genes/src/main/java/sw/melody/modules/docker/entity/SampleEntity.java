2be3f625134d88da8f6d5d1628e2f8de24be2c3b
melody <137987284@qq.com>
2018-08-13 18:10:21 +0800

    检索条件

--- m2018-genes/src/main/java/sw/melody/modules/docker/entity/SampleEntity.java
+++ m2018-genes/src/main/java/sw/melody/modules/docker/entity/SampleEntity.java
@@ -14,19 +14,29 @@ import java.util.Date;
 @Getter
 public class SampleEntity implements Serializable {
     private Long id;
+
     // 病人编码，新增时自动生成，格式：HY+时间戳
+
     private Long sickId;
     private String originName;
     // 病人姓名
+
     @NotBlank(message="参数名不能为空")
     private String location;
     //上传时间
+
     private Date uploadTime;
+
     // 病情描述
+
     private String handlerStatus;
+
     //更新时间
+
     private Date handlerTime;
+
     //触发时间
+
     private Date triggerTime;
     private String sickName;
     private String sickCode;
